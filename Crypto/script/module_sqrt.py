d6d5d2cd85501706cdc53211be8a954d0afce792
hacmao <37682486+hacmao@users.noreply.github.com>
2020-03-15 08:39:32 +0700

    Update module_sqrt.py

--- Crypto/script/module_sqrt.py
+++ Crypto/script/module_sqrt.py
@@ -63,3 +63,14 @@ def modular_sqrt(a, p):
         x = (x * gs) % p
         b = (b * g) % p
         r = m
+
+ def legendre_symbol(a, p):
+    """ Compute the Legendre symbol a|p using
+        Euler's criterion. p is a prime, a is
+        relatively prime to p (if p divides
+        a, then a|p = 0)
+        Returns 1 if a has a square root modulo
+        p, -1 otherwise.
+    """
+    ls = pow(a, (p - 1) // 2, p)
+    return -1 if ls == p - 1 else ls
