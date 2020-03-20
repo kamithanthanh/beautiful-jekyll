bb0f48ac78d9da6a1a7fe2ff48a9d45f9143a24e
hacmao <37682486+hacmao@users.noreply.github.com>
2020-03-20 18:42:56 +0700

    Create get.sh

---
+++ server/get.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/hacmao/hacmao.github.io/raw/master/server/Dockerfile
+git clone https://github.com/hacmao/hacmao.github.io/raw/master/server/Dockerfile
+git clone https://github.com/hacmao/hacmao.github.io/raw/master/server/xinetd.conf
