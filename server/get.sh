9d2b9d1774eb706039546e45df8f7a8c7e615795
hacmao <37682486+hacmao@users.noreply.github.com>
2020-04-01 08:31:40 +0700

    Update get.sh

--- server/get.sh
+++ server/get.sh
@@ -2,3 +2,4 @@ wget https://github.com/hacmao/hacmao.github.io/raw/master/server/Dockerfile
 wget https://github.com/hacmao/hacmao.github.io/raw/master/server/service.conf
 wget https://github.com/hacmao/hacmao.github.io/raw/master/server/init.sh
 wget https://github.com/hacmao/hacmao.github.io/raw/master/server/run.sh
+wget https://github.com/hacmao/hacmao.github.io/raw/master/server/build.sh
