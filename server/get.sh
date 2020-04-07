249969a68c422ee0b0844d6057e716c69067ff10
hacmao <37682486+hacmao@users.noreply.github.com>
2020-04-07 12:30:44 +0700

    Update get.sh

--- server/get.sh
+++ server/get.sh
@@ -3,4 +3,5 @@ wget https://github.com/hacmao/hacmao.github.io/raw/master/server/service.conf
 wget https://github.com/hacmao/hacmao.github.io/raw/master/server/init.sh
 wget https://github.com/hacmao/hacmao.github.io/raw/master/server/run.sh
 wget https://github.com/hacmao/hacmao.github.io/raw/master/server/build.sh
+wget https://github.com/hacmao/hacmao.github.io/raw/master/server/config.py
 chmod +x build.sh
